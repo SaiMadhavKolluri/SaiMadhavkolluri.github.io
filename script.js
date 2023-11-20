c70a73e5054cb7371b1b18bad9ac613c660af519
Sai Madhav Kolluri <sai@exuminstruments.com>
2023-11-20 14:35:51 -0700

    Test 19

--- script.js
+++ script.js
@@ -10,4 +10,7 @@ const onProgress = (event) => {
     progressBar.classList.remove('hide');
   }
 };
+const modelViewerTransform = document.querySelector("model-viewer#transform");
+  
+modelViewerTransform.scale = `${0.5} ${0.5} ${0.5}`;
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
